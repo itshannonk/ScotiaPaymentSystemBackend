476df5de767173bfdc9ad109231d004d235f7ae2
Grace Tsao <47228306+gracet9182@users.noreply.github.com>
2019-11-23 16:22:54 -0500

    authentication is done in python

--- model/DataSet.py
+++ model/DataSet.py
@@ -1,5 +1,8 @@
 from firebase import firebase
 import json
+from model import Invoice
 firebase = firebase.FirebaseApplication("https://csc207-tli.firebaseio.com/")
+
+invoiceObject = Invoice()
 resultPut = firebase.put('Business Owner','this is where the authID goes', {'Name':'Grace', 'help':'me'})
 print(resultPut)
