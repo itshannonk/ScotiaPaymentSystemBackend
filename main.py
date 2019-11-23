56863c1d12135a10916fdc509e42855c0cf3320b
Shannon Komguem <s.komguem@mail.utoronto.ca>
2019-11-23 15:03:11 -0500

    Created basic flask app for testing purposes

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+
+# if __name__ == "__main__":
+#    app.run(debug=True)
