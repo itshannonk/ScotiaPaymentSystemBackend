a5081f4abcb0a5175ee51d6249e7108d1603f306
Shannon Komguem <s.komguem@mail.utoronto.ca>
2019-11-23 20:02:10 -0500

    changes to main.py

--- model/DataRetrieval.py
+++ model/DataRetrieval.py
@@ -1,6 +1,6 @@
 def get_pls_work():
     from firebase import firebase
     firebase = firebase.FirebaseApplication('https://csc207-tli.firebaseio.com/', None)
-    result = firebase.get('/CocaCola', 'testing')
+    result = firebase.get('/CocaCola', 'hcO40l8tnZVXkUVVYPq5QQJBoM92')
     return result
     print(result)
