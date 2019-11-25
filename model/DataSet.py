25faebee176237bffc1d013d70e3e999ccc7e847
Grace Tsao <47228306+gracet9182@users.noreply.github.com>
2019-11-25 13:34:38 -0500

    sample code to database storage

--- model/DataSet.py
+++ model/DataSet.py
@@ -9,7 +9,7 @@ firebase = firebase.FirebaseApplication("https://csc207-tli.firebaseio.com/")
 invoiceObject = Invoice.Invoice()
 JSONversion = json.dumps(invoiceObject.__dict__)
 print(JSONversion)
-resultPut = firebase.put('Business Owner','this is where the authID goes', {'Name':'Grace', 'help':'me', 'Invoice':{0:JSONversion, 1:JSONversion}})
+resultPut = firebase.put('Invoices','FEkg7hBAVxPgbwHHp2VmNwVCCwK2', {0:{"price":12.99, "status": "True"}, 1:{"price":12.99, "status": "True"}})
 #this is how you delete something. comment out the line below and see what it in firebase before uncommenting it out
 result = firebase.delete("Business Owner/this is where the authID goes", "Name")
 #this shows how you can update and add more children. Comment out the line below to see it
