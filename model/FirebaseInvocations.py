6475866192fa8d4041d38c523af668a8dd85f499
chickenmilkcream <zhiting.peng@mail.utoronto.ca>
2019-11-27 19:58:08 -0500

    pls pls work

--- model/FirebaseInvocations.py
+++ model/FirebaseInvocations.py
@@ -211,7 +211,8 @@ def get_assigned_invoices(userID: str):
 
         inventorydb = DATABASE.get(customer_path, None)
         for key in inventorydb:
-            listOfCustomerIDs += str(key) + ":" + str(inventorydb[key]) + ','
+            listOfCustomerIDs += key + ":" + DATABASE.get('Business Owner', key) + ","
+
         return listOfCustomerIDs[:-1]
     except:
         return ""
